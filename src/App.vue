de95d38f24b1f01926877b7716ea99bd24c8e6e3
hiroki1216 <g.o.b96just.uw@icloud.com>
2023-10-16 22:32:30 +0900

    Headerをコンポーネントに分割

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { nextTick, ref } from 'vue'
+import Header from './pages/Header.vue'
 const count = ref(1);
 const increment = async ()=> {
   count.value++
@@ -8,17 +9,7 @@ const increment = async ()=> {
 </script>
 
 <template>
-  <header>
-    <a href="index.html"><img src="../src/assets/logo.jpeg" height="75"  alt="ウェブサンプル株式会社"></a>
-    <nav>
-      <ul>
-        <li><a href="#">HOME</a></li>
-        <li><a href="#">ABOUT</a></li>
-        <li><a href="#">WORKS</a></li>
-        <li><a href="#">CONTACT</a></li>
-      </ul>
-    </nav>
-  </header>
+  <Header></Header>
   <main>
     <p>{{ count }}</p>
     <button @click="increment">カウントアップ</button>
@@ -26,36 +17,4 @@ const increment = async ()=> {
 </template>
 
 <style scoped>
-header{
-  display: flex;
-  border-bottom: 1px solid #dcd9d9;
-  }
-nav{
-  width: 100%;
-  height :75px;
-  display: flex;
-  justify-content: space-around;
-}
-ul{
-  width: 50%;
- display: flex;
- justify-content: space-around;
- font-weight: bold;
-}
-li{
-  color: black;
-  list-style: none;
-  margin: auto 0;
-}
-a:hover {
-  color: aqua;
-}
-
-a {
-color:inherit;
- text-decoration: none;
-}
-img{
-  border-radius: 10px;
-}
 </style>
---
+++ src/pages/Header.vue
@@ -0,0 +1,49 @@
+<script setup>
+</script>
+<template>
+    <header>
+        <a href="index.html"><img src="../assets/logo.jpeg" height="75"  alt="H.Sプロフィールページ"></a>
+        <nav>
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">WORKS</a></li>
+                <li><a href="#">CONTACT</a></li>
+            </ul>
+        </nav>
+  </header>
+</template>
+<style scoped>
+header{
+  display: flex;
+  border-bottom: 1px solid #dcd9d9;
+  }
+nav{
+  width: 100%;
+  height :75px;
+  display: flex;
+  justify-content: space-around;
+}
+ul{
+  width: 50%;
+ display: flex;
+ justify-content: space-around;
+ font-weight: bold;
+}
+li{
+  color: black;
+  list-style: none;
+  margin: auto 0;
+}
+a:hover {
+  color: aqua;
+}
+
+a {
+color:inherit;
+ text-decoration: none;
+}
+img{
+  border-radius: 10px;
+}
+</style>
