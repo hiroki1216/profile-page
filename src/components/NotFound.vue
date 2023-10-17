e4526c239eeb0571f37675efbf060d523bb9db9f
hiroki1216 <g.o.b96just.uw@icloud.com>
2023-10-17 21:00:18 +0900

    404Not Foundのページとリダイレクト処理を追加

---
+++ src/components/NotFound.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+<template>
+    <div class="message">
+        <p class="main">404 Not Found.</p>
+        <p class="sub">Sorry page not found...</p>
+        <img src="@/assets/logo.jpeg" alt="" width="500" height="">
+</div>
+</template>
+<style scoped>
+.message .main{
+    margin: 10vh auto 0;
+    text-align: center;
+    font-size:50px;
+}
+.message .sub{
+    text-align: center;
+    font-size:30px;
+}
+.message img{
+    margin: auto;
+    display: block;
+    border-radius: 30px;
+}
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import NotFound from '@/components/NotFound.vue'
 import Home from '@/components/Home.vue'
 import About from '@/components/About.vue'
 import Works from '@/components/Works.vue'
 import Contact from '@/components/Contact.vue'
 
 const routes = [
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
     { path: '/', component: Home },
     { path: '/about', component: About },
     { path: '/works', component: Works },
